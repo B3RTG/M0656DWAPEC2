f5e62807e0b37eb1ec444abf181e85fe26cfb031
B3RTG <albert.kais@gmail.com>
2021-03-09 19:36:22 +0100

    Ejercicio 1 - Every

--- PEC2_Ej1/d_every/core.js
+++ PEC2_Ej1/d_every/core.js
@@ -2,14 +2,14 @@
 // are even numbers.
 
 function allEven(input) {
-  return input;
+  return input.every(x => (x % 2) == 0);
 }
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType(input) {
-  return input;
+  return input.every((x, _, a) => typeof x === typeof a[0]);
 }
 
 // Check to see if every element in the matrix is
@@ -17,14 +17,14 @@ function allSameType(input) {
 // greater than 0.
 
 function positiveMatrix(input) {
-  return input;
+  return input.every(x=> Array.isArray(x) && x.every(y=> y > 0));
 }
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels(input) {
-  return input;
+  return input.every(x=> typeof x == 'string' && x.match(/[aeiou]/gi).every( (y,i,a)=> y == a[0]))
 }
 
 module.exports = {
