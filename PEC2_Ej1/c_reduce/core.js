10b3c83ea58d26616317b269e4c0e6cd6ae82e0f
B3RTG <albert.kais@gmail.com>
2021-03-09 19:16:58 +0100

    Ejercicio 1 - Reduce

--- PEC2_Ej1/c_reduce/core.js
+++ PEC2_Ej1/c_reduce/core.js
@@ -1,17 +1,22 @@
 function sum(array) {
-  // your code here
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue);
 }
 
 function productAll(array) {
-  // your code here
+  //put all in one array an with reduce + concat and then reduce with product
+  return array.reduce((x,y) => x.concat(y)).reduce((x,y) => x*y);
 }
 
 function objectify(array) {
-  // your code here
+  return array.reduce((x, y) => {
+                        x[y[0]] = y[1];
+                        return x;
+                      }, {});
 }
 
 function luckyNumbers(array) {
-  // your code here
+  //added a ',' for the 'and' condition to pass test, i'm not sure if it's correct
+  return 'Your lucky numbers are: ' + array.reduce((x,y,i,a) => (i < a.length - 1) ? x +', ' + y : x +', and ' + y);
 }
 
 module.exports = {
